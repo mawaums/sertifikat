935aadd1e8bf73862dfdd30703de2c20f5a106dd
Kemahasiswaan UMS <kemahasiswaan.ums@gmail.com>
2017-06-09 11:37:10 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="bulma.css" rel="stylesheet">
+        <title>Sertifikat</title>
+    </head>
+    <body>
+        <section class="section">
+            <div class="container">
+                <div class="columns">
+                    <div class="column is-3">
+                        <h1 class="title is-1">Upload Sertifikat</h1>
+                    </div>
+                </div>
+                <div class="columns">
+                    <div class="column is-3">
+                        <form>
+                            <div class="field">
+                                <label class="label">Jenis</label>
+                                <p class="control">
+                                    <span class="select">
+                                        <select>
+                                            <option>Akademik</option>
+                                            <option>Minat Bakat</option>
+                                        </select>
+                                    </span>
+                                </p>
+                            </div>
+                            <div class="field">
+                                <label class="label">Nama Kompetisi</label>
+                                <p class="control">
+                                    <input class="input" type="text" placeholder="Nama Kompetisi">
+                                </p>
+                            </div>
+                            <div class="field">
+                                <label class="label">Tingkat</label>
+                                <p class="control">
+                                    <span class="select">
+                                        <select>
+                                            <option>Propinsi</option>
+                                            <option>Nasional</option>
+                                            <option>Internasional</option>
+                                        </select>
+                                    </span>
+                                </p>
+                            </div>
+                            <div class="field">
+                                <label class="label">Nama Pemenang</label>
+                                <p class="control">
+                                    <input class="input" type="text" placeholder="Nama Pemenang">
+                                </p>
+                            </div>
+                            <div class="field">
+                                <label class="label">Prestasi</label>
+                                <p class="control">
+                                    <input class="input" type="text" placeholder="Prestasi">
+                                </p>
+                            </div>
+                            <div class="field is-grouped">
+                                <p class="control">
+                                    <button class="button is-primary">Upload</button>
+                                </p>
+                                <p class="control">
+                                    <button class="button">Batal</button>
+                                </p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
